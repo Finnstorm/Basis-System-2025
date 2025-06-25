accba332e3c64972a39a7b929de6c3c7de4e8898
Andreas <manzandreas29@hotmail.com>
2025-06-25 10:03:28 +0200

    MAP

---
+++ assets/graphics/Tilemap_1_Lilli_Testa.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tilemap_1_Lilli_Testassets" tilewidth="32" tileheight="32" tilecount="91" columns="13">
+ <image source="../../../../../Downloads/Tilemap_1_Lilli_Testassets.png" width="416" height="224"/>
+</tileset>
