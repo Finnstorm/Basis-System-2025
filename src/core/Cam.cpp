77093dc796007e9bd3da49f6667d432efa6802ad
Finn Kruse <krusefinn@icloud.com>
2025-07-06 22:48:26 +0200

    Alle Branches Gemerged funktional mache jetzt die erweiterung

--- src/core/Cam.cpp
+++ src/core/Cam.cpp
@@ -19,5 +19,4 @@ void Cam::Cam_Movement(double dtm) {
     Vector2 mpp=pao.Get_Player_Pos();
     this->cam.target.x=mpp.x;
     this->cam.target.y=mpp.y;
-    std::cout<<cam.target.x<<"  "<<cam.target.y<<"\n";
 }
