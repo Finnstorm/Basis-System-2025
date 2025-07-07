87cc7088fa5a6d8bf3b8678052b1866182f324ac
Finn Kruse <71725853+Finnstorm@users.noreply.github.com>
2025-07-07 13:04:08 +0200

    Update GameScene.cpp
    fix 1

--- src/scenes/GameScene.cpp
+++ src/scenes/GameScene.cpp
@@ -16,36 +16,23 @@ using namespace std::string_literals;
 
 game::scenes::GameScene::GameScene()
 {
+   {
     dtm.Start();
     cam=std::make_shared<Cam>(this->mp);
+    screen.Load_Tiled_Objects(objectManager,p_cm);
     screen.Load_Tiled_Objects(objectManager);
 
     // Your scene initialization code here...
 
-}
+ void game::scenes::GameScene::Update()
 
-game::scenes::GameScene::~GameScene()
-{
-    // Your scene cleanup code here...
-}
-
-void game::scenes::GameScene::Update()
-{
-    // Your process input and update game scene code here...
-    if (IsKeyPressed(KEY_ESCAPE))
-        game::core::Store::stage->SwitchToNewScene("pause"s, std::make_unique<PauseScene>());
-    if (IsKeyPressed(KEY_L)){
-        ToggleFullscreen();
-    }
-    mp.Tick(dtm.Get_Dt());
-
-
+    this->cam->Cam_Movement(dtm.Get_Dt());
+    objectManager.Cleanup_Objects();
     p_cm->Check_Collisions();
     dtm.Update();
 }
 
-void game::scenes::GameScene::Draw()
-{
+ void game::scenes::GameScene::Draw()
     BeginDrawing();
     ClearBackground(WHITE);
     screen.Draw_Level(this->cam);
