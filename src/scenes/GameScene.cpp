e143fa4e71bf75cc616c1a3674cbd5d144fb728c
Finn Kruse <71725853+Finnstorm@users.noreply.github.com>
2025-07-07 13:02:06 +0200

    Update GameScene.cpp
    fix?

--- src/scenes/GameScene.cpp
+++ src/scenes/GameScene.cpp
@@ -39,8 +39,7 @@ void game::scenes::GameScene::Update()
     }
     mp.Tick(dtm.Get_Dt());
 
-    this->cam->Cam_Movement(dtm.Get_Dt());
-    objectManager.Cleanup_Objects();
+
     p_cm->Check_Collisions();
     dtm.Update();
 }
