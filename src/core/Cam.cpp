0a2b60a3de3ae4bb9c22d586c8b517b8faef9b0d
Will <Willi.Becker@stud.srh-university.de>
2025-07-12 19:59:02 +0200

    Hallo

--- src/core/Cam.cpp
+++ src/core/Cam.cpp
@@ -1,19 +1,26 @@
-//
-// Created by Kruse on 06/05/2025.
-//
-
 #include "Cam.h"
-#include "raymath.h"
+#include <raylib.h>
 #include "../config.h.in"
+#include "raymath.h"
+
+Cam::Cam(Player_Base_Class& mp) : pao(mp)
+{
+    this->cam = {0};
+    this->cam.zoom = game::Config::cam_zoom; 
+}
 
 void Cam::Cam_Movement(double dtm, Vector2 mapDimensions) {
     Vector2 mpp = pao.Get_Player_Center();
     this->cam.target = mpp;
+
     this->cam.offset = { (float)GetScreenWidth() / 2.0f, (float)GetScreenHeight() / 2.0f };
+
     this->cam.zoom = game::Config::cam_zoom;
+
     if (mapDimensions.x > 0 && mapDimensions.y > 0) {
         float halfScreenWidthInWorld = (float)GetScreenWidth() / 2.0f / this->cam.zoom;
         float halfScreenHeightInWorld = (float)GetScreenHeight() / 2.0f / this->cam.zoom;
+
         this->cam.target.x = Clamp(this->cam.target.x, halfScreenWidthInWorld, mapDimensions.x - halfScreenWidthInWorld);
         this->cam.target.y = Clamp(this->cam.target.y, halfScreenHeightInWorld, mapDimensions.y - halfScreenHeightInWorld);
     }
